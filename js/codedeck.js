de8a720c3aca18588069bfb45b6d42c124278e89
Pascal Rettig <pascal@cykod.com>
2011-08-15 11:18:58 -0400

    Initial WIP

---
+++ js/codedeck.js
@@ -0,0 +1,32 @@
+
+
+$(document).ready(function() {
+
+  var JavaScriptMode = require("ace/mode/javascript").Mode;
+
+
+ function focusCallback() {
+   disableKeyboardEvents = true;
+  }
+
+  function blurCallback() {
+    disableKeyboardEvents = false;
+  }
+
+  $(document).bind("slideenter",function(e) {
+    var current = $(".current");
+    current.find(".code").each(function() {
+      if(!$(this).hasClass('codeEditor')) {
+        var editor = ace.edit(this.id);
+        $(this).addClass('codeEditor');
+        editor.getSession().setMode(new JavaScriptMode());
+
+        editor.on('focus', focusCallback);
+        editor.on('blur', blurCallback);
+      }
+    });
+
+  });
+
+});
+
